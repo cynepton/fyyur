71997c90f71c942ca6f461535328f4106d11efe8
isaac <aderonmuisaac@gmail.com>
2020-07-09 13:19:58 +0100

    feat: connected to a local database

--- app.py
+++ app.py
@@ -13,18 +13,18 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from flask_migrate import Migrate
+import psycopg2
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 moment = Moment(app)
+# TODO: connect to a local postgresql database
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
-
-# TODO: connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
 # Models.
