5b1dd415aaec1ed76894f5a6458ceb020ac1e611
isaac <aderonmuisaac@gmail.com>
2020-07-11 13:08:47 +0100

    feat: add artist search

--- app.py
+++ app.py
@@ -312,13 +312,21 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
+  artist_select = Artist.query
+  artist_list = []
+
+  artists = artist_select.filter(Artist.name.ilike('%' + request.form['search_term'] + '%'))
+
+  for artist in artists:
+    artist_list.append({
+      "id":artist.id,
+      "name":artist.name,
       "num_upcoming_shows": 0,
-    }]
+    })
+
+  response={
+    "count": len(artist_list),
+    "data": artist_list
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
