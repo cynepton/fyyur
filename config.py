77d948e2f6ec3640d835b3a658d0d33529c7fd79
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-12-18 17:46:35 +0100

    chore: do setup

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://cynepton:cynepton@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://doadmin:adxi23ks43tnh995@testcluster-do-user-8414461-0.b.db.ondigitalocean.com:25060/fsnd?sslmode=require'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
